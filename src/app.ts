2ab2d1ba9e8da2628c47259c4f46a4b685a9c905
safin33221 <safin33221@gmail.com>
2025-08-01 21:09:11 +0600

    implement not found middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { router } from './app/routes'
 import { globalErrorHandler } from './app/middleware/globalErrorHandler'
+import { notFound } from './app/middleware/notFound'
 
 const app = express()
 
@@ -19,4 +20,6 @@ app.get('/', (req: Request, res: Response) => {
 
 
 app.use(globalErrorHandler)
+
+app.use(notFound)
 export default app
