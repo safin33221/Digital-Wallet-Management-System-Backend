ef16b969920ad109918c6c49dc03495eabc7451e
safin33221 <safin33221@gmail.com>
2025-08-02 16:28:18 +0600

    Completed check auth middleware

---
+++ src/app/middleware/checkAuth.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import AppError from "../errorHelpers/AppError";
+import statusCode from "../utils/statusCode";
+import { envVar } from "../config/env.config";
+import { JwtPayload } from "jsonwebtoken";
+import { verifyToken } from "../utils/jwt";
+import { User } from "../modules/user/user.model";
+
+
+export const checkAuth = (...authRole: string[]) => async (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+
+        const accessToken = req.headers.authorization
+        if (!accessToken) {
+            throw new AppError(statusCode.UNAUTHORIZED, "No token Received")
+        }
+        const verifiedToken = verifyToken(accessToken, envVar.JWT_SECRET) as JwtPayload
+
+        const user = await User.findOne({ email: verifiedToken.email })
+
+        if (!user) {
+            throw new AppError(statusCode.NOT_FOUND, "User not found")
+        }
+
+        if (!authRole.includes(verifiedToken.role)) {
+            throw new AppError(403, "You'r not permitted to view this route")
+        }
+        next()
+
+    } catch (error) {
+        next(error)
+    }
+}
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
+import { checkAuth } from "../../middleware/checkAuth";
+import { Role } from "./user.interface";
 
 const router = Router()
 
 router.post("/create", userController.createUser)
-router.get("/", userController.getUsers)
+router.get("/", checkAuth(Role.ADMIN), userController.getUsers)
 
-export const UserRoutes = router
+export const UserRoutes = router 
