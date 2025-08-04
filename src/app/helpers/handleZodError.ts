7e3bdcacbcafd2bc600e242b371148ae6bd2a69d
safin33221 <safin33221@gmail.com>
2025-08-04 21:39:20 +0600

    handle zod error

--- src/app/helpers/handleZodError.ts
+++ src/app/helpers/handleZodError.ts
@@ -3,14 +3,15 @@ import { IErrorSource, IGenericErrorResponse } from "../interface/error.type";
 
 export const handleZodError = (err: any): IGenericErrorResponse => {
     const errorSources: IErrorSource[] = []
-
-    err.issues.array.forEach((issue: any) => {
+    
+    err.issues.forEach((issue: any) => {
         errorSources.push({
             path: issue.path.length > 1 && issue.path.reverse().join("inside"),
             message: issue.message,
-
+            
         })
     });
+    console.log(errorSources);
     return {
         statusCode: 400,
         message: "Zod Error",
--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -5,6 +5,8 @@ import { NextFunction, Request, Response } from "express";
 import { envVar } from "../config/env.config";
 import AppError from "../errorHelpers/AppError";
 import { handleDuplicateError } from "../helpers/handleDuplicateError";
+import { handleZodError } from "../helpers/handleZodError";
+import { IErrorSource } from "../interface/error.type";
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     if (envVar.NODE_DEV === 'DEVELOPMENT') {
@@ -12,14 +14,18 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
     }
     let statusCode = 500
     let message = `something went wrong`
+    let errorSources: IErrorSource[] = []
 
     if (err.code === 1100) {
         const simplified = handleDuplicateError(err)
         statusCode = simplified.statusCode;
         message = simplified.message
     }
-    else if(err.name === 'ZodError'){
-        
+    else if (err.name === 'ZodError') {
+        const simplified = handleZodError(err)
+        statusCode = simplified.statusCode;
+        message = simplified.message
+        errorSources = simplified.errorSources as IErrorSource[]
     }
     else if (err instanceof AppError) {
         statusCode = err.statusCode
