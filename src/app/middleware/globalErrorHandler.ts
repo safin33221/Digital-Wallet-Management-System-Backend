dccc2ccfe327d798809952f010a34b3e5ef6c6ce
safin33221 <safin33221@gmail.com>
2025-08-04 21:08:37 +0600

    Update global error with duplicate error

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -4,6 +4,7 @@
 import { NextFunction, Request, Response } from "express";
 import { envVar } from "../config/env.config";
 import AppError from "../errorHelpers/AppError";
+import { handleDuplicateError } from "../helpers/handleDuplicateError";
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     if (envVar.NODE_DEV === 'DEVELOPMENT') {
@@ -12,13 +13,18 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
     let statusCode = 500
     let message = `something went wrong`
 
-
-    if (err instanceof AppError) {
+    if (err.code === 1100) {
+        const simplified = handleDuplicateError(err)
+        statusCode = simplified.statusCode;
+        message = simplified.message
+    }
+ 
+    else if (err instanceof AppError) {
         statusCode = err.statusCode
         message = err.message
     }
 
-    if (err instanceof Error) {
+    else if (err instanceof Error) {
         statusCode = 500
         message = err.message
     }
